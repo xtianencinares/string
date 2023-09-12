d156692b52210302ac5e39b1e49ef75d66022f96
Christian Encinares Bocz <144127538+xtianencinares@users.noreply.github.com>
2023-09-12 10:01:46 +0800

    Create string.cpp
    string c++ last name

---
+++ string.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+main()
+{
+       ifstream encinares ("input.txt");
+       string str;
+      
+       while (getline(encinares,str))
+      {
+      cout<<str<<endl;
+      
+            }
+      system("pause");
+              }
